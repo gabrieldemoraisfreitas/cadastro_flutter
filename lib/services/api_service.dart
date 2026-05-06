f51751b709d7609ccc40629cb47355379aec3097
gabrieldemoraisfreitas <gabriel.m.freitas10@gmail.com>
2026-05-06 20:25:41 -0300

    Teste API com erro

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import '../models/usuario.dart';
 
 class ApiService {
-  static const String _baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const String _baseUrl = 'https://jsonplaceholder.typicode.com/erro';
 
   Future<List<Usuario>> buscarUsuarios() async {
     final uri = Uri.parse('$_baseUrl/users');
